3ae0fbac80d5240526f2e5f9b768b8c21c238bc4
Luis Escamilla <157620028+LuisOEscamilla@users.noreply.github.com>
2026-09-10 20:34:31 -0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        leftMost = [0] * len(height)
+        rightMost = [0] * len(height)
+
+        leftMost[0] = height[0]
+        for i in range(1, len(height)):
+            leftMost[i] = max(leftMost[i-1], height[i])
+        rightMost[len(height)-1] = height[len(height)-1]
+        for i in range(len(height)-2,-1,-1):
+            rightMost[i] = max(rightMost[i+1], height[i])
+        result = 0
+        for i in range(len(height)):
+            result += (min(leftMost[i],rightMost[i]) -height[i])
+
+
+        return result
